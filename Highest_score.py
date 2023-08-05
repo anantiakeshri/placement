6da58444c2fc67c7ad7218da432c708970aabad8
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-05 17:34:55 +0530

    Highest score program

---
+++ Highest_score.py
@@ -0,0 +1,14 @@
+# Checking highest score in class without using max() function
+
+student_scores = input("Input a list of student scores:\n").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}") 
+
+# Code by Anantia Keshri
