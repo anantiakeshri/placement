07440dc0eef8990de600a710b89801383d676d9f
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-12 15:57:21 +0530

    Testing Ceaser cipher game

--- Ceaser_Cipher.py
+++ Ceaser_Cipher.py
@@ -1,30 +1,40 @@
-#Testing game module
+#This game is about encoding snd decoding message using shift amount
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# from Cipher_art import logo
+# print(logo)
 
-def encrypt(plain_text, shift_num):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_pos = position + shift_num
-        new_letter = alphabet[new_pos]
-        cipher_text += new_letter
-    print(f"The cipher text is {cipher_text}")
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+# shift = shift % 26
 
-def decrypt(cipher_text, shift_number):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_number
-        plain_text += alphabet[new_position]
-    print (f"The decrypted text is {plain_text}")
-          
-if direction == 'encode':
-    encrypt(plain_text=text, shift_num=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_number=shift)
+def ceaser(plain_text, shift_amount, cipher_direction):
+    start_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in plain_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            start_text += alphabet[new_position]
+        else: 
+            start_text += char
+    print(f"The {cipher_direction}d text is {start_text}.")
     
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    ceaser(plain_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    result = input("Do you want to continue? Type 'yes' to continue and 'no' to stop.\n").lower()
+    if result == "no":
+        should_continue = False
+        print("Bye, have a nice day!")
+        
+#This code is written by Anantia Keshri
