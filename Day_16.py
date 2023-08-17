058b19ea68eb0fac795e7d609506b983eac66636
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-18 01:22:05 +0530

    Day-16

---
+++ Day_16.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+kachua = Turtle()
+
+kachua.shape("turtle")
+kachua.color("red", "yellow")
+kachua.forward(100)
+print(kachua)
+
+parda = Screen()
+parda.exitonclick()
+print(parda.canvheight())
+
+kachua.color("pink")
+print(kachua)
