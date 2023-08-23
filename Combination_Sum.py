b60e66a5b3ea527b9e416528cb49b76d40dade59
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-23 23:59:36 +0530

    COmbination Sum - Leet Code question

--- Combination_Sum.py
+++ Combination_Sum.py
@@ -11,7 +11,23 @@ target = 7
 # Output: [[2,2,2,2],[2,3,3],[3,5]]
 
 def combinationSum(candidates, target):
-    res = []
-    for i in range (len(candidates)):
+    res=[]
+    
+    def dfs(i, curr, total):
+        if total == target:
+            res.append(curr.copy())
+            return
         
+        if i >= len(candidates) or total > target:
+            return
+
+        curr.append(candidates[i])
+        dfs(i, curr, total + candidates[i])
+        curr.pop()
+        dfs(i+1, curr, total)
+
+    dfs(0, [], 0)
+    return res
+        
+
 print(combinationSum(candidates, target))
