61cd71caee0a904b2c83156d8a087ae91ae0d145
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-23 23:51:27 +0530

    Contains Duplicate - LeetCode

--- contains_Duplicate.py
+++ contains_Duplicate.py
@@ -1,17 +1,16 @@
 nums = [1,2,3,4,5,1,2,3]
 
-# print(len(nums))
-# print(len(set(nums)))
-# print(set(nums))
 
-def containDuplicates(nums):
-    num_set = []
+def containsDuplicate(nums):
+    return len(set(nums))!=len(nums)
+
+    num_set = set()
     
     for i in nums:
         if i in num_set:
             return True
         else:
-            num_set.add[i]
+            num_set.add(i)
     return False
-            
-print(containDuplicates(nums))
+
+print(containsDuplicate(nums))
