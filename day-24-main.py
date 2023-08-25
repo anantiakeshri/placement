ba89b56c71b53671c5f6149e6f37f64a614ee134
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-25 23:50:29 +0530

    Day 24 main.py file

--- day-24-main.py
+++ day-24-main.py
@@ -6,6 +6,10 @@
 
 # file.close()
 
+# with open('day-24.txt', mode="a") as file:
+#     file.write("\nMy favourite food is Pasta.")
+
+# By default mode stays in read.
 with open('day-24.txt') as file:
-    contents = file.read()
+    contents = file.read()    
     print(contents)
