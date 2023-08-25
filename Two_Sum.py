c2213bb4505d038f6aa7ae0fc56f80a3af2385fa
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-25 23:04:48 +0530

    Two Sum - Leet Code

--- Two_Sum.py
+++ Two_Sum.py
@@ -8,9 +8,10 @@ def twoSum(nums, target):
     for i, num in enumerate(nums):
         if target - num in seen:
             return([seen[target - num], i])
+        
         elif num not in seen:
             seen[num] = i
-            
+        
 print(twoSum(nums, target))
 
 # Explanation:
