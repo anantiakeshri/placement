2f6ee0223b8f4597d41b3e366bddb38cd5c05fb7
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-28 05:02:39 +0530

    Tkinter module

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -1,11 +1,29 @@
-import tkinter 
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(500, 300)
 
-my_label = tkinter.Label(text="I am a Label", font=("Courier", 24, "bold"))
-my_label.pack(side="top")
+my_label = Label(text="I am a Label", font=("Courier", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.pack()
+
+""" Introducing Button """
+def button_clicked():
+    new_text = entry.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+""" Introducing Entry"""
+entry = Entry(width=20)
+entry.pack()
+
 
 
 
