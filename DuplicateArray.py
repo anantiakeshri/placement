c6f667db18fee69b6e22fdf529ef9c0aa0fe4892
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-06 23:28:21 +0530

    Removing Duplicate Values from array

--- DuplicateArray.py
+++ DuplicateArray.py
@@ -5,14 +5,29 @@
 nums = [1,2,1,3,4,5,5]
 
 def removeDuplicates(nums):
-    if len(nums) == 0:
-        return 0 
+    # Create an empty set to store unique elements.
+    unique_set = set()
     
-    j = 0
-    for i in range(1, len(nums)):
-        if nums[i] != nums[j]:
-            j += 1
-            nums[j] = nums[i]
-    return j + 1
+    # Create a new list to store the unique elements in the order of appearance.
+    unique_list = []
+    
+    for num in nums:
+        if num not in unique_set:
+            # If the element is not in the set, it's unique.
+            # Add it to the set and the new list.
+            unique_set.add(num)
+            unique_list.append(num)
+    
+    # Copy the unique elements back to the original list if needed.
+    # You can also return the unique_list if you don't want to modify the original list.
+    nums[:] = unique_list
+    
+    # Return the length of the modified list with duplicates removed.
+    return len(unique_list)
 
-print(removeDuplicates(nums))
+# Example usage:
+nums = [1, 2, 1, 3, 4, 5, 5]
+result = removeDuplicates(nums)
+print(nums)  # The modified list with duplicates removed
+print(result)  # The length of the modified list
+
