cfde81a7388a27089ef742a8acc9244a4c6fb221
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-11 16:37:44 +0530

    Leet Code - Goal Parser - Ques 1678

--- Goal_Parser.py
+++ Goal_Parser.py
@@ -1,18 +1,18 @@
 #1678 - Leet Code
 
-class Solution:
-    def interpret(self, command: str) -> str: 
-        s=""
-        for i in range(len(command)):
-            if command[i]=='G':
-                s+=command[i]
-            elif c[i]=='(' and command[i+1]==")":
-                s+='o'
-            elif command[i]=='(' and command[i+1]=='a':
-                s+='al'
-            else:
-                pass
-        print (s)
-    
 command = "G()(al)"
 
+def interpret(command): 
+    s=""
+    for i in range(len(command)):
+        if command[i]=='G':
+            s+=command[i]
+        elif command[i]=='(' and command[i+1]==")":
+            s+='o'
+        elif command[i]=='(' and command[i+1]=='a':
+            s+='al'
+        else:
+            pass
+    print (s)
+    
+print(interpret(command))
