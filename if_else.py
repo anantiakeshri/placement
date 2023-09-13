aadc946988e2f5967d6961b4e9d8dc4693d64758
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-13 22:08:45 +0530

    Number of days in a month - IF ELSE

--- if_else.py
+++ if_else.py
@@ -33,46 +33,46 @@
 
 
 # // Write a program to accept a number between 1-12 and display name of the month and days in that month like 1 for Jan and num of days 31.. so on
-# Num = int(input("Enter number between 1 to 12: "))
+Num = int(input("Enter number between 1 to 12: "))
 
-# if Num == 1:
-#     print("January")
-#     print("Number of days 31")
-# elif Num == 2:
-#     print("Feburary")
-#     print("Number of days in normal year is 28, else in leap year 29")
-# elif Num == 3:
-#     print("Mar")
-#     print("Number of days 31")
-# elif Num == 4:
-#     print("April")
-#     print("Number of days 30")
-# elif Num == 5:
-#     print("May")
-#     print("Number of days 31")
-# elif Num == 6:
-#     print("June")
-#     print("Number of days 30")
-# elif Num == 7:
-#     print("July")
-#     print("Number of days 31")
-# elif Num == 8:
-#     print("Aug")
-#     print("Number of days 31")
-# elif Num == 9:
-#     print("Sept")
-#     print("Number of days 30")
-# elif Num == 10:
-#     print("Oct")
-#     print("Number of days 31")
-# elif Num == 11:
-#     print("Nov")
-#     print("Number of days 30")
-# elif Num == 12:
-#     print("Dec")
-#     print("Number of days 31")
-# else:
-#     print("Number from 1 to 12")
+if Num == 1:
+    print("January")
+    print("Number of days 31")
+elif Num == 2:
+    print("Feburary")
+    print("Number of days in normal year is 28, else in leap year 29")
+elif Num == 3:
+    print("Mar")
+    print("Number of days 31")
+elif Num == 4:
+    print("April")
+    print("Number of days 30")
+elif Num == 5:
+    print("May")
+    print("Number of days 31")
+elif Num == 6:
+    print("June")
+    print("Number of days 30")
+elif Num == 7:
+    print("July")
+    print("Number of days 31")
+elif Num == 8:
+    print("Aug")
+    print("Number of days 31")
+elif Num == 9:
+    print("Sept")
+    print("Number of days 30")
+elif Num == 10:
+    print("Oct")
+    print("Number of days 31")
+elif Num == 11:
+    print("Nov")
+    print("Number of days 30")
+elif Num == 12:
+    print("Dec")
+    print("Number of days 31")
+else:
+    print("Number from 1 to 12")
 
 
 
