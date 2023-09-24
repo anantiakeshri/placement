7041f06b069a7f3565a52f66bb322e8b573b3876
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-24 14:02:38 +0530

    IS Palindrome - 2 pointer approach

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,10 +1,22 @@
 
-S = 'malayalam'
+S = 'programming'
 
-def isPalindrome(S):
-    if S == S[::-1]:
+# def isPalindrome(S):
+#     if S == S[::-1]:
+#         return 1
+#     else:
+#         return 0
+
+# print(isPalindrome(S))
+
+def palindrome(S):
+    i = 0
+    j = len(S) - 1
+    while (i < j):
+        if S[i] != S[j]:
+            return 0
+        i = i + 1
+        j = j - 1
         return 1
-    else:
-        return 0
-
-print(isPalindrome(S))
+    
+print(palindrome(S))
