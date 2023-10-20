827ccc3d78f57bd24ad3721e68b4060b4d808ecd
anantiakeshri <476cs18007.rp@gmail.com>
2023-10-20 11:36:24 +0530

    Highest Score without using Max function

--- Highest_score.py
+++ Highest_score.py
@@ -1,5 +1,4 @@
 # Checking highest score in class without using max() function
-
 student_scores = input("Input a list of student scores:\n").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
